6a91a457fb17df5359300d12fbbb1cf85127d993
Jelle Raaijmakers <jelle@gmta.nl>
2021-04-26 22:26:09 +0200

    Serenity: Implement mouse cursor support
    Supports hiding the cursor and showing system cursors.

--- src/video/serenity/SDL_serenityvideo.h
+++ src/video/serenity/SDL_serenityvideo.h
@@ -25,6 +25,58 @@
 
 #include "../SDL_sysvideo.h"
 
+#include <LibGUI/Widget.h>
+#include <LibGUI/Window.h>
+
+class SerenitySDLWidget final : public GUI::Widget {
+    C_OBJECT(SerenitySDLWidget)
+public:
+    explicit SerenitySDLWidget(SDL_Window*);
+    RefPtr<Gfx::Bitmap> m_buffer;
+
+protected:
+    void paint_event(GUI::PaintEvent&) override;
+    void resize_event(GUI::ResizeEvent&) override;
+    void show_event(GUI::ShowEvent&) override;
+    void hide_event(GUI::HideEvent&) override;
+
+    void mousedown_event(GUI::MouseEvent&) override;
+    void mousemove_event(GUI::MouseEvent&) override;
+    void mouseup_event(GUI::MouseEvent&) override;
+
+    void keydown_event(GUI::KeyEvent& event) override;
+    void keyup_event(GUI::KeyEvent& event) override;
+
+    void enter_event(Core::Event&) override;
+    void leave_event(Core::Event&) override;
+
+private:
+    SDL_Window* m_sdl_window = nullptr;
+};
+
+class SerenityPlatformWindow final {
+public:
+    SerenityPlatformWindow(SDL_Window* sdl_window)
+        : m_window(GUI::Window::construct())
+        , m_widget(SerenitySDLWidget::construct(sdl_window))
+    {
+        m_window->resize(sdl_window->w, sdl_window->h);
+        m_window->set_resizable(false);
+    }
+
+    static SerenityPlatformWindow* from_sdl_window(SDL_Window* window)
+    {
+        return static_cast<SerenityPlatformWindow*>(window->driverdata);
+    }
+
+    NonnullRefPtr<GUI::Window> window() { return m_window; }
+    NonnullRefPtr<SerenitySDLWidget> widget() { return m_widget; }
+
+private:
+    NonnullRefPtr<GUI::Window> m_window;
+    NonnullRefPtr<SerenitySDLWidget> m_widget;
+};
+
 #endif /* SDL_serenityvideo_h_ */
 
 /* vi: set ts=4 sw=4 expandtab: */
